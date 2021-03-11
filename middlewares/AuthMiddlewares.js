bd412ab9943f52de7d3ed47b988c2f261bc35d9d
bijaydas <bijay@ligious.com>
2021-03-12 01:32:39 +0530

    [Added] AuthMiddleware

--- middlewares/AuthMiddlewares.js
+++ middlewares/AuthMiddlewares.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-module.exports.isValidDPMToken = (req, res, next) => {
-  const token = req.headers['X-DPM-Token'];
-  if (token && token === process.env.TOKEN) {
+module.exports.isValidDXToken = (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token && token === process.env['x-token']) {
     return next();
   }
   return res.status(403).json({
