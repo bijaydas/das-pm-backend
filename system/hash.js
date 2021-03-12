5af2abb585ad86b7c087aaa284e8d5ab2091f698
bijaydas <bijay@ligious.com>
2021-03-12 19:07:28 +0530

    [Added] hash.js

--- system/hash.js
+++ system/hash.js
@@ -5,3 +5,7 @@ const saltRounds = 10;
 module.exports.hashPassword = (plainPassword) => {
   return bcrypt.hashSync(plainPassword, saltRounds);
 };
+
+module.exports.comparePassword = (plainPassword, hashedPassword) => {
+  return bcrypt.compareSync(plainPassword, hashedPassword);
+};
