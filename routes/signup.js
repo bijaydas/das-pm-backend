daeeb82c45f30b39a836f6f1d3e09662e6a5dd66
bijaydas <bijay@ligious.com>
2021-03-12 18:25:32 +0530

    [Added] Email exists validation

--- routes/signup.js
+++ routes/signup.js
@@ -6,12 +6,29 @@ const {isValidDXToken} = require('../middlewares/authMiddlewares');
 const {quickSignupValidation} = require('../middlewares/requestsMiddlewares');
 const {User} = require('../models');
 const {hashPassword} = require('../system/hash');
+const {Op} = require('sequelize');
 
 // Overwall API auth with Das Password Manager Token
 router.use(isValidDXToken);
 
 router.post('/quick', quickSignupValidation, async (req, res) => {
   const {email, password} = req.body;
+  const emailExists = await User.findOne({
+    where: {
+      [Op.and]: [
+        {primary_email: email},
+        {active: '1'},
+      ],
+    },
+  });
+
+  if (emailExists) {
+    return res.status(401).json({
+      message: 'Email already exists',
+      status: 401,
+    });
+  }
+
   const payload = {
     primary_email: email,
     password: hashPassword(password),
